13fba863a35c9c4b84c2ee183929549fd9e5c3f1
Deepak <deepakviji5556@gmail.com>
2025-06-22 18:28:45 +0530

    Merge branch 'main' of https://github.com/Deepak5556/LearnFirebaseWithFlutter into CRUD

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
   @override
