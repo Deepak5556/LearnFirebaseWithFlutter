ab1b5e15b93cb75095463dbe07fa0d57fa18ae1d
Deepak <deepakviji5556@gmail.com>
2025-06-22 17:46:04 +0530

    Text Edited

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Helgfvlo Welcome Deepak!'))),
+      home: Scaffold(body: Center(child: Text('Hello Welcome Deepak!'))),
     );
   }
 }
